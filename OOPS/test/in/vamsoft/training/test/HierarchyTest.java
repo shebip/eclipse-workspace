72eb31b170a0afb90864a54e836595c04d45d9e4
shebip <shebi.fs@gmail.com>
2017-09-15 17:52:53 +0530

    commit1

---
+++ OOPS/test/in/vamsoft/training/test/HierarchyTest.java
@@ -0,0 +1,13 @@
+package in.vamsoft.training.test;
+
+import in.vamsoft.training.SportsCar;
+
+public class HierarchyTest {
+
+	public static void main(String[] args) {
+		SportsCar car=new SportsCar();
+		System.out.println(car);
+
+	}
+
+}
